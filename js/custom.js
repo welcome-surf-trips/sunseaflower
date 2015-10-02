755e24e1bf8d40b48316135f20386accf9a02d46
Alberth <alberth_david13@hotmail.com>
2015-10-02 09:09:22 -0300

    Ajustes

--- js/custom.js
+++ js/custom.js
@@ -32,7 +32,7 @@ $('.navbar-collapse ul li a').click(function() {
 $(document).ready(function() {
             $("#joinville").addClass("ativo");
             $(".label-titulo").text("09-OUT-2015");
-            $(".label-sub-titulo").text("LOCAL A SER DEFINIDO");
+            $(".label-sub-titulo").text("Bovary Snooker Pub");
                 
             $("a[rel=example_group]").fancybox();
             
